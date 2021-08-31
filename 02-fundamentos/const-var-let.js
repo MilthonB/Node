f875d3e58f842456f404b880e016d31d33424e8c
MB <milthonbor@gmail.com>
2021-08-31 14:44:14 -0700

    const, let, var

---
+++ 02-fundamentos/const-var-let.js
@@ -0,0 +1,13 @@
+
+// var es para variables globales
+// const su valor no cambia son más ligeras
+// var no utilizar 
+var nombre = "Lorenzo";
+
+
+if( true ){ 
+  let  nombre = "Perez"
+}
+
+
+console.log(nombre);
