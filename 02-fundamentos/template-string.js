d4fdd7e6f544ad47e45a454b4d1e5905896bd0e8
MB <milthonbor@gmail.com>
2021-08-31 14:49:49 -0700

    más pruebas template string

--- 02-fundamentos/template-string.js
+++ 02-fundamentos/template-string.js
@@ -4,8 +4,21 @@ const nombre = 'DeadPool';
 const real = 'Wade Winston';
 
 
-const template = nombre+" "+real;
-const template = `${nombre} ${real}`;
+const normal = nombre+" "+real;
+const template1 = `${nombre} ${real}`;
+const template2 = `${nombre} ${ 1 + 2}`;
+const template3 = `${ 1 === 1}`;
 
 
-console.log(template)
+console.log(template1)
+console.log(template2)
+console.log(template3)
+
+
+const html = `
+    <h1>Hola</h1>
+    <p>Mundo</p>
+`;
+
+
+console.log(html);
