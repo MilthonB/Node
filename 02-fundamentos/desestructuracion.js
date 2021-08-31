8313a34d0aa8058cdac3041d8db3ced025b6ee32
MB <milthonbor@gmail.com>
2021-08-31 15:00:32 -0700

    arerglos y objetos desestructuracion

--- 02-fundamentos/desestructuracion.js
+++ 02-fundamentos/desestructuracion.js
@@ -3,6 +3,7 @@
 const deadpool = {
     nombre: 'Wade',
     apellido: 'Winston',
+    // edad: 50,
     poder: 'Regeneracion',
     getNombre() {
         return `${this.nombre} ${this.apellido}`
@@ -15,6 +16,36 @@ const deadpool = {
 // const poder = deadpool.poder;
 // console.log(nombre, apellido, poder);
 
-let {nombre, apellido, poder} = deadpool; 
+// let {nombre, apellido, poder, edad = 0} = heroe; 
+    
+//     console.log(nombre, apellido, poder, edad);
 
-console.log(nombre, apellido, poder);
+
+function imprimeHeroe( { nombre, apellido, poder, edad = 0 } ){
+    
+    // const {nombre, apellido, poder, edad = 0} = heroe; 
+    
+    console.log(nombre, apellido, poder, edad);
+
+
+}
+
+
+// imprimeHeroe(deadpool);
+
+const heroes = ['DeadPool', 'SuperMan', 'Batman'];
+
+
+// const h1 = heroes[0];
+// const h2 = heroes[1];
+// const h3 = heroes[2];
+
+// console.log(h1);
+// console.log(h2);
+// console.log(h3);
+
+
+const [ h1,h2,h3 ] = heroes;
+const [ ,,h3S ] = heroes;
+console.log(h1,h2,h3);
+console.log(h3S);
