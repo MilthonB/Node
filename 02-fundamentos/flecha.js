48db8545e9181b197ae225c8841f47a1e34da226
MB <milthonbor@gmail.com>
2021-08-31 15:04:28 -0700

    funcion de flecha

---
+++ 02-fundamentos/flecha.js
@@ -0,0 +1,14 @@
+
+
+
+// function sumar( a,b = 10 ){
+//     return a + b;
+// }
+
+// const sumar = ( a,b=10 ) => {
+//     return a + b;
+// };
+
+const sumar = ( a,b=10 ) => a + b;
+
+console.log( sumar(5) );
