f421c5ea188c8c32d29857122e742697d5105453
MB <milthonbor@gmail.com>
2021-08-31 17:50:14 -0700

    require

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,13 +1,22 @@
 
+const fs = require('fs');
+
 console.clear();
 
 console.log('==================')
 console.log('   Tabla del 5   ')
 console.log('==================')
 
-
+let salida = '';
 const base = 5;
 
 for( let i = 1; i <= 10; i++ ){
-    console.log( `${base} X ${i} = ${base*i}` )
+    salida += `${base} X ${i} = ${base*i}\n`;
 }
+
+console.log(salida)
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if(err) throw err
+    console.log(`tabla-${base}`)
+});
