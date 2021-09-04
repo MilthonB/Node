4cd644873256d8cb07344d692b49866785597273
MB <milthonbor@gmail.com>
2021-09-04 16:27:37 -0700

    servir un sitio web completo en public

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -3,17 +3,21 @@ const app = express();
 const port = 8080;
 
 
-app.use( express.static('public') );
+app.use(express.static('public'));
 
 
-app.get('/hola-mundo',  (req, res) => {
-  res.send('Hello World')
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
 })
 
-app.get( '*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html')
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
 })
- 
-app.listen( port , (req, res) => {
-    console.log(`Corriendo en el puerto ${ port }`);
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/old/404.html')
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Corriendo en el puerto ${port}`);
 })
