fa64d5dc0c8b7608eb2c9d1c17d236cbe3b2a2fb
MB <milthonbor@gmail.com>
2021-09-03 18:49:55 -0700

    buscar ciudad

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -16,6 +16,20 @@ const main = async () => {
         switch (opt) {
             case 1:
                 //Buscar ciudad
+                //Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ')
+                console.log(lugar); 
+                //Buscar los lugares
+                // / Seleccionar el luebas
+                //Datos del clima
+                //mostar los resultados
+                console.log('\nInformación de la ciuda\n'.green);
+                console.log('Ciudad:',);
+                console.log('Lat:',);
+                console.log('Lng:',);
+                console.log('Temperaturas:',);
+                console.log('Mínima:',);
+                console.log('Máxima:',);
                 break;
 
             case 2:
---
+++ 05-clima-app/models/busquedas.js
@@ -0,0 +1,22 @@
+
+
+class Busquedas {
+
+    historial = [];
+
+    constructor(){
+
+    }
+
+    async ciudad( lugar="" ){
+        //Peticion http
+        console.log(lugar);
+
+        return []; //retornar los lugares 
+    }
+
+}
+
+
+
+module.exports = Busquedas;
