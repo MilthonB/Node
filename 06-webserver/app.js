c1f718bfe1bf080157ac27fdc00e7ff53774a59e
MB <milthonbor@gmail.com>
2021-09-04 16:10:38 -0700

    ejemplo de rutas y carpeta publica

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express();
 const port = 8080;
- 
-app.get('/', function (req, res) {
+
+
+app.use( express.static('public') );
+
+
+app.get('/hola-mundo',  (req, res) => {
   res.send('Hello World')
 })
+
+app.get( '*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html')
+})
  
-app.listen( port , () => {
+app.listen( port , (req, res) => {
     console.log(`Corriendo en el puerto ${ port }`);
 })
