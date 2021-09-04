5d596789957b2a66800ce271275b959ff464ea7d
MB <milthonbor@gmail.com>
2021-09-03 18:38:40 -0700

    mostrar menu

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,12 +1,34 @@
-const {leerInput} = require('./helpers/inquire')
- 
+const { leerInput, inquirerMenu, pausa } = require('./helpers/inquire')
 
 
 
 
-const main = async() => {
-    const texto = await leerInput('Carga algo');
-    console.log(texto);
+
+const main = async () => {
+
+
+    let opt;
+
+    do {
+
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                //Buscar ciudad
+                break;
+
+            case 2:
+                //Historial
+                break;
+            case 0:
+                //Salir
+                break;
+        }
+
+        await pausa();
+    } while (opt != 0);
+
 
 }
 
