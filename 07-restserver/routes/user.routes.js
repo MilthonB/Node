f37c462f7cf0ca59dfbc7dc87cc88ef4d0333934
MB <milthonbor@gmail.com>
2021-09-05 21:24:04 -0700

    agregando un nuevo middleware roles

--- 07-restserver/routes/user.routes.js
+++ 07-restserver/routes/user.routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator')
 
 const { validarCampos } = require('../middlewares/validar-campos');
+const { esRolValido } = require('../middlewares/validar-rol');
 const Role = require('../models/role');
 
 
@@ -16,12 +17,7 @@ route.post('/',[
     check('nombre','El nombre es obligatorio').not().isEmpty(),
     check('correo','El correo no es válido').isEmail(),
     check('password','El password debe de conterner 6 o más caracteres').isLength({min:6}),
-    check('rol').custom( async(rol = '') => {
-        const existeRol = await Role.findOne({rol});
-        if( !existeRol ){
-            throw new Error(`El rol ${rol} no está registrado en la BD`);
-        }
-    }),
+    check('rol').custom( esRolValido ),
     validarCampos
 ] ,ctrl.usuariosPost);
 
