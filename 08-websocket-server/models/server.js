0baeefcf8a013dee44c1f335e9641a944a448fe9
MB <milthonbor@gmail.com>
2021-09-13 21:53:34 -0700

    instalacionde socket.io y config del server

--- 08-websocket-server/models/server.js
+++ 08-websocket-server/models/server.js
@@ -5,8 +5,10 @@ const cors = require('cors');
 class Server {
 
     constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
+        this.app    = express();
+        this.port   = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io     = require('socket.io')(this.server);
 
         this.paths = {
         };
@@ -36,7 +38,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             console.log(`El servidor esta corriendo en el puerto: ${this.port}`);
         });
     }
