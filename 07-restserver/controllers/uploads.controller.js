79db987e39b470054921dbf6595cd63a0bae43b1
MB <milthonbor@gmail.com>
2021-09-13 17:53:25 -0700

    actulizar img de producto y usuario

--- 07-restserver/controllers/uploads.controller.js
+++ 07-restserver/controllers/uploads.controller.js
@@ -1,9 +1,10 @@
 const { response, request } = require("express");
 const { subirArchivo } = require("../helpers/subir-archivo");
+const { Usuario, Producto } = require("../models");
 
 
 
-const cargarArchivo = async(req, res = response) => {
+const cargarArchivo = async (req, res = response) => {
 
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
@@ -12,19 +13,44 @@ const cargarArchivo = async(req, res = response) => {
     }
 
     //Imagenes
-    const nombre = await subirArchivo(req.files,undefined,'imgs')
-  
-    res.status(200).json({nombre});
+    const nombre = await subirArchivo(req.files, undefined, 'imgs')
+
+    res.status(200).json({ nombre });
 
 }
 
-const actualizarIMG = (req=request, res=response) => {
+const actualizarIMG = async(req = request, res = response) => {
+
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+        res.status(400).json({ msg: ' No hay archivo para subir ' });
+        return;
+    }
 
     const { coleccion, id } = req.params;
 
-    res.json({
-        coleccion,
-        id
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuario':
+            modelo = await Usuario.findById(id);
+            break;
+        case 'producto':
+            modelo = await Producto.findById(id);
+            break;
+        default:
+            return res.status(500).json({
+                msg: `La coleccion ${coleccion} no se ha agregado`
+            });
+    }
+
+
+
+    const nombre = await subirArchivo(req.files, undefined, coleccion)
+
+    modelo.img = nombre;
+
+    res.status(200).json({
+        modelo
     })
 
 
