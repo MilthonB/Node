3abcf16727db5b967b46c73c4c6fd8021629380d
MB <milthonbor@gmail.com>
2021-09-15 00:57:52 -0700

    inicio de la pantalla de los escritorios

--- 09-socket-colas/public/js/escritorio.js
+++ 09-socket-colas/public/js/escritorio.js
@@ -1,3 +1,43 @@
+// Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button');
+
+ const searchParams = new URLSearchParams(window.location.search); // obtener los parametros del url
+
+ if( !searchParams.has('escritorio') ){ // si en los parametros recibidos no tiene el elemento escritorio  
+    window.location = "index.html";
+    throw new Error('El escritorio es obligatorio');
+ }
+
+ const escritorio = searchParams.get('escritorio'); // obtener lo que contiene el parametro escritorio;
 
 
-console.log('Escritorio HTML');
+ const socket = io();
+
+
+socket.on('connect', () => {
+    btnAtender.disabled = false;
+});
+
+
+socket.on('disconnect', () => {
+    
+    btnAtender.disabled = true;
+});
+
+// socket.on( 'ultimo-ticket', ( ultimo ) => {
+//     lblNuevoTicket.innerText = "Ticket: "+ultimo;
+// });
+
+
+
+btnAtender.addEventListener( 'click', () => {
+
+    // socket.emit('siguiente-ticket',null, (ticket) => {
+    //     console.log('Hola mundo ', ticket);
+    //     lblNuevoTicket.innerText = ticket;
+
+    // })
+
+
+});
