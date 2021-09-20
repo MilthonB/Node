3f9f1b2d0fd6274a9d3889be2c2d6195fefe7b9f
MB <milthonbor@gmail.com>
2021-09-19 22:52:59 -0700

    delete poryecto errores

--- 12-socket-chat-(Diseño mejorado)/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config(); // necesario para usar los env en todo el servidor
-
-const Server = require('./models/server'); // Server 
-
-const server = new Server();//Nueva instancia
-
-
-server.listen();//Inicializacion del server 
